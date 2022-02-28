f2da533429cd8a5ade4c487292b450bb47cdb860
Bilel Bouach <bilel.boach@gmail.com>
2022-02-27 23:40:35 -0500

    added app.tsx

---
+++ src/app.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+import { VehicleContainer } from "./pages/vehicle/view/VehicleContainer";
+import "./scss/style.css";
+
+export const App: React.FC = () => <VehicleContainer />;
+
+export default App;
