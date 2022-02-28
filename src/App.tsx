d32406dd17464c1c549d03211d9bab2509a1d6de
Bilel Bouach <bilel.boach@gmail.com>
2022-02-27 23:39:36 -0500

    deleted App.tsx

--- src/App.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import { VehicleContainer } from "./pages/vehicle/view/VehicleContainer";
-import "./scss/style.css";
-
-export const App: React.FC = () => <VehicleContainer />;
-
-export default App;
